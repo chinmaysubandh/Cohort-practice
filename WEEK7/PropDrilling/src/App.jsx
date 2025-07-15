6c75a9214c781594e04643ff5b1203f19cd3b0a4
chinmaysubandh <91725938+chinmaysubandh@users.noreply.github.com>
2025-07-15 23:35:32 +0530

    recoil

--- WEEK7/PropDrilling/src/App.jsx
+++ WEEK7/PropDrilling/src/App.jsx
@@ -1,62 +1,20 @@
 import { useRecoilValue, useRecoilState } from "recoil";
 import TodoList from "./components/TodoList";
 import { CountAtom } from "./store/atoms/count";
+import Count from "./components/Count";
 
 function App() {
+  
   return (
     <div className="bg-gray-950 text-white h-screen w-screen flex items-center justify-center">
       <div className="bg-gray-900 sm:mx-16 max-sm:mx-4 lg:mx-24 h-full w-full items-center">
-        <Count />
+        <Count/>
         <TodoList/>
       </div>
     </div>
   );
 }
 
-function Count() {
-  return (
-    <div className="flex flex-col items-center justify-center">
-      <CountRenderer />
-      <Buttons />
-    </div>
-  );
-}
 
-function CountRenderer() {
-  
-  const count = useRecoilValue(CountAtom);
-  console.log(count);
-
-  return <div>
-    {count}
-  </div>;
-}
-
-function Buttons() {
-   
-  const [count, setCount] = useRecoilState(CountAtom);
-
-  return (
-    <div className="flex flex-col items-center gap-4">
-      <button
-        className="px-4 py-2 rounded-sm mx-2 text-center text-2xl bg-blue-900 h-10 w-32"
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Increase
-      </button>
-
-      <button
-        className="px-4 py-2 rounded-sm mx-2 text-center text-2xl bg-blue-900 h-10 w-32"
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        Decrease
-      </button>
-    </div>
-  );
-}
 
 export default App;
---
+++ WEEK7/PropDrilling/src/components/Count.jsx
@@ -0,0 +1,49 @@
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { CountAtom } from "../store/atoms/count";
+
+export default function Count() {
+    console.log("re-render");
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <CountRenderer />
+            <Buttons />
+        </div>
+    );
+}
+
+function CountRenderer() {
+
+    const count = useRecoilValue(CountAtom);
+
+
+    return <div>
+        {count}
+    </div>;
+}
+
+function Buttons() {
+
+    const setCount = useSetRecoilState(CountAtom);
+
+    return (
+        <div className="flex flex-col items-center gap-4">
+            <button
+                className="flex px-4 py-2 rounded-sm mx-2 items-center  text-2xl bg-blue-900 h-10 w-32"
+                onClick={() => {
+                    setCount(count => count+1)
+                }}
+            >
+                Increase
+            </button>
+
+            <button
+                className="flex items-center px-4 py-2 rounded-sm mx-2  text-2xl bg-blue-900 h-10 w-32"
+                onClick={() => {
+                    setCount(count => count -1)
+                }}
+            >
+                Decrease
+            </button>
+        </div>
+    );
+}
