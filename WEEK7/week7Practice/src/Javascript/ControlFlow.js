c9e14417fd90f704fb4dce741539e032349d6f60
chinmaysubandh <91725938+chinmaysubandh@users.noreply.github.com>
2025-08-16 00:30:06 +0530

    js basics

--- WEEK7/week7Practice/src/Javascript/ControlFlow.js
+++ WEEK7/week7Practice/src/Javascript/ControlFlow.js
@@ -1,4 +1,5 @@
-let a = [2,34,322];
+//Switch Case
+{ /* let a = [2,34,322];
 
 switch (typeof a) {
     case "number":
@@ -20,4 +21,27 @@ switch (typeof a) {
     default:
         console.log("other")
         break;
-}
+} */}
+//For loop
+ {/*let array = []
+
+for (let i = 0; i < 4; i++){
+    console.log(i);
+    
+    
+    for (let j = 0; j < 3; j++){
+        console.log('inner loop' + j);
+        
+        
+    }
+    
+    array[i] = 1 + i
+    console.log(array);
+    
+    
+    
+    
+    
+} */}
+
+//while loop
